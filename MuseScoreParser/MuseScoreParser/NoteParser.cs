00440a647c270fad03ec20aefb92cf3bce0bcd31
Benjamin Krug <benjaminkrug@yahoo.com>
2022-09-18 16:40:11 -0400

    Extract functions

--- MuseScoreParser/MuseScoreParser/NoteParser.cs
+++ MuseScoreParser/MuseScoreParser/NoteParser.cs
@@ -19,31 +19,38 @@ namespace MuseScoreParser
                 new List<IAsmSymbol>()
             };
             var measureNumber = 0;
-            var labelSuffix = 'A';
+            var repeatSuffix = 'A';
             foreach (var measure in measures)
             {
-                var foundVoices = GetVoicesInMeasure(measure);
-                var durationOfMeasure = foundVoices.First().Sum(n => n.Notes.First().Duration);
-                var measureOfRests = new List<INote> { new Rest { Duration = durationOfMeasure } };
-                var voice1 = foundVoices.Any() ? GetSingleNoteInChord(foundVoices.First(), 0) : measureOfRests;
-                var voice3 = foundVoices.Count > 1 ? GetSingleNoteInChord(foundVoices.Last(), 0) : measureOfRests;
-                var voiceWithChords = foundVoices.FirstOrDefault(v => v.Any(c => c.Notes.Count > 1));
-                var voice2 = voiceWithChords != null ? GetSingleNoteInChord(voiceWithChords, 1) : measureOfRests;
-                var repeat = measure.Descendants("barline")?.Descendants("repeat")?.FirstOrDefault();
-
-                if (voice1.Sum(n => n.Duration) != durationOfMeasure
-                    || voice2.Sum(n => n.Duration) != durationOfMeasure
-                    || voice3.Sum(n => n.Duration) != durationOfMeasure)
-                    throw new Exception($"Unequal duration in measure {measureNumber}");
-
+                GroupNotesByVoice(measureNumber, measure, out var voices, out var repeat);
                 ++measureNumber;
-                AddNotesOfOneMeasure(shortLabel + "1" + labelSuffix, allNotes[0], measureNumber, voice1, repeat);
-                AddNotesOfOneMeasure(shortLabel + "2" + labelSuffix, allNotes[1], measureNumber, voice2, repeat);
-                AddNotesOfOneMeasure(shortLabel + "3" + labelSuffix, allNotes[2], measureNumber, voice3, repeat);
+                AddNotesOfOneMeasure(shortLabel + "1" + repeatSuffix, allNotes[0], measureNumber, voices[0], repeat);
+                AddNotesOfOneMeasure(shortLabel + "2" + repeatSuffix, allNotes[1], measureNumber, voices[1], repeat);
+                AddNotesOfOneMeasure(shortLabel + "3" + repeatSuffix, allNotes[2], measureNumber, voices[2], repeat);
                 if (repeat != null)
-                    ++labelSuffix;
+                    ++repeatSuffix;
             }
             return allNotes;
+        }
+
+        private static void GroupNotesByVoice(int measureNumber, XElement measure, out List<List<INote>> notesByVoice, out XElement repeat)
+        {
+            var foundVoices = GetVoicesInMeasure(measure);
+            var durationOfMeasure = foundVoices.First().Sum(n => n.Notes.First().Duration);
+            var measureOfRests = new List<INote> { new Rest { Duration = durationOfMeasure } };
+
+            var voice1 = foundVoices.Any() ? GetSingleNoteInChord(foundVoices.First(), 0) : measureOfRests;
+            var voice3 = foundVoices.Count > 1 ? GetSingleNoteInChord(foundVoices.Last(), 0) : measureOfRests;
+            var voiceWithChords = foundVoices.FirstOrDefault(v => v.Any(c => c.Notes.Count > 1));
+            var voice2 = voiceWithChords != null ? GetSingleNoteInChord(voiceWithChords, 1) : measureOfRests;
+            notesByVoice = new List<List<INote>> { voice1, voice2, voice3 };
+
+            if (voice1.Sum(n => n.Duration) != durationOfMeasure
+                || voice2.Sum(n => n.Duration) != durationOfMeasure
+                || voice3.Sum(n => n.Duration) != durationOfMeasure)
+                throw new Exception($"Unequal duration in measure {measureNumber}");
+
+            repeat = measure.Descendants("barline")?.Descendants("repeat")?.FirstOrDefault();
         }
 
         private static void AddNotesOfOneMeasure(string label, List<IAsmSymbol> voice, int measureNumber, List<INote> notes, XElement repeat)
