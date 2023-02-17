7eef9a3eb536007368c79e123061957db04305ed
Benjamin Krug <benjaminkrug@yahoo.com>
2023-02-17 06:26:08 -0500

    Added test for chord scenario

--- MuseScoreParser/MusicXmlParser.Tests/NoteParserTests.cs
+++ MuseScoreParser/MusicXmlParser.Tests/NoteParserTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using MuseScoreParser;
+using MuseScoreParser.Enums;
 using MuseScoreParser.Models;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -26,7 +27,7 @@ namespace MusicXmlParser.Tests
         }
 
         [Test]
-        public void Parse_XmlContainsOnePartAndOneVoicePerMeasureAndNoChords_GetListOfNotes()
+        public void Parse_XmlContainsOnePartAndOneVoicePerMeasureAndNoChords_Success()
         {
             const string SOURCE_XML =
 @"<?xml version=""1.0"" encoding=""UTF-8""?>
@@ -107,6 +108,99 @@ namespace MusicXmlParser.Tests
                                         Chords = new List<NewChord>
                                         {
                                             GenerateSingleNoteChord("B", "", "3", "24"),
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            //Act
+            var actualObject = new NewNoteParser().Parse(SOURCE_XML);
+
+            //Assert
+            actualObject.Should().BeEquivalentTo(expectedObject);
+        }
+
+        [Test]
+        public void Parse_XmlContainsChords_Success()
+        {
+            const string SOURCE_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<score-partwise version=""3.1"">
+    <part>
+        <measure>
+            <note>
+                <pitch>
+                    <step>G</step>
+                    <octave>3</octave>
+                </pitch>
+                <duration>6</duration>
+                <voice>1</voice>
+            </note>
+            <note>
+                <chord/>
+                <pitch>
+                    <step>E</step>
+                    <octave>3</octave>
+                </pitch>
+                <duration>6</duration>
+                <voice>1</voice>
+            </note>
+            <note>
+                <chord/>
+                <pitch>
+                    <step>C</step>
+                    <octave>3</octave>
+                </pitch>
+                <duration>6</duration>
+                <voice>1</voice>
+            </note>
+        </measure>
+    </part>
+</score-partwise>";
+            var expectedObject = new List<NewPart>
+            {
+                new NewPart
+                {
+                    Measures = new List<NewMeasure>
+                    {
+                        new NewMeasure
+                        {
+                            Voices = new Dictionary<string, NewVoice>
+                            {
+                                {
+                                    "1",
+                                    new NewVoice
+                                    {
+                                        Chords = new List<NewChord>
+                                        {
+                                            new NewChord
+                                            {
+                                                Notes = new List<NewNote>
+                                                {
+                                                    new NewNote
+                                                    {
+                                                        Step = "G",
+                                                        Octave = "3",
+                                                        Duration = "6"
+                                                    },
+                                                    new NewNote
+                                                    {
+                                                        Step = "E",
+                                                        Octave = "3",
+                                                        Duration = "6"
+                                                    },
+                                                    new NewNote
+                                                    {
+                                                        Step = "C",
+                                                        Octave = "3",
+                                                        Duration = "6"
+                                                    }
+                                                }
+                                            }
                                         }
                                     }
                                 }
