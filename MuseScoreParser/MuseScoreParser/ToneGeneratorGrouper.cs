70b0fdda924889661e0f319a6b4c3c5093caafa2
Benjamin Krug <benjaminkrug@yahoo.com>
2023-03-03 22:03:40 -0500

    Added code to start converting parsed notes to GeneratorNotes

--- MuseScoreParser/MuseScoreParser/ToneGeneratorGrouper.cs
+++ MuseScoreParser/MuseScoreParser/ToneGeneratorGrouper.cs
@@ -1,5 +1,8 @@
-﻿using MuseScoreParser.Models;
+﻿using MuseScoreParser.Enums;
+using MuseScoreParser.Models;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace MuseScoreParser
 {
@@ -7,7 +10,50 @@ namespace MuseScoreParser
     {
         internal List<ToneGenerator> GetToneGenerators(List<NewPart> parsedParts)
         {
-            return new List<ToneGenerator>();
+            var measures = parsedParts.Single().Measures.Select(m => m.Voices.Single().Value).ToList();
+            var generatorNotes = new List<GeneratorNote>();
+            for (var measureNumber = 1; measureNumber <= measures.Count; ++ measureNumber)
+            {
+                var notesInMeasure = measures[measureNumber - 1].Chords
+                    .Select(c => c.Notes.Single())
+                    .Select(n => new GeneratorNote
+                     {
+                         StartMeasure = measureNumber,
+                         EndMeasure = measureNumber,
+                         Pitch = GetPitch(n)
+                     });
+                generatorNotes.AddRange(notesInMeasure);
+            }
+            return new List<ToneGenerator>()
+            {
+                new ToneGenerator
+                {
+                    GeneratorNotes = generatorNotes.ToList()
+                }
+            };
+        }
+
+        private static ReadOnlyDictionary<string, int> _notesWithinOctive =
+            new ReadOnlyDictionary<string, int>(
+                new Dictionary<string, int>
+                {
+                    { "C", 0 },
+                    { "D", 2 },
+                    { "E", 4 },
+                    { "F", 5 },
+                    { "G", 7 },
+                    { "A", 9 },
+                    { "B", 11 },
+                }
+            );
+
+        //TODO: Add tests for invalid data
+        private Pitch GetPitch(NewNote parsedNote)
+        {
+            var pitchInt = (int.Parse(parsedNote.Octave) - 1) * 12 + 3;
+            var alterInt = string.IsNullOrWhiteSpace(parsedNote.Alter) ? 0 : int.Parse(parsedNote.Alter);
+            var withinOctiveInt = _notesWithinOctive[parsedNote.Step.ToUpper()] + alterInt;
+            return (Pitch)(pitchInt + withinOctiveInt);
         }
     }
 }
---
+++ MuseScoreParser/MusicXmlParser.Tests/PartBuilder.cs
@@ -0,0 +1,181 @@
+﻿using MuseScoreParser.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicXmlParser.Tests
+{
+    class PartBuilder
+    {
+        private Dictionary<(string part, string voice), List<NewVoice>> _voices = new Dictionary<(string part, string voice), List<NewVoice>>();
+
+        public PartBuilder AddPartAndVoice(string part, string voice)
+        {
+            _voices.Add((part, voice), new List<NewVoice>());
+            return this;
+        }
+
+        public PartBuilder AddMeasureOfOneNoteChords(string part, string voice)
+        {
+            _voices[(part, voice)].Add(new NewVoice
+            {
+                Chords = new List<NewChord>
+                {
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                Step = "G",
+                                Alter = "-1",
+                                Octave = "3",
+                                Duration = "6"
+                            }
+                        }
+                    },
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                Step = "D",
+                                Alter = "1",
+                                Octave = "3",
+                                Duration = "12"
+                            }
+                        }
+                    }
+                }
+            });
+            return this;
+        }
+
+        public PartBuilder AddMeasureOfThreeNoteChords(string part, string voice)
+        {
+            _voices[(part, voice)].Add(new NewVoice
+            {
+                Chords = new List<NewChord>
+                {
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                Step = "A",
+                                Alter = string.Empty,
+                                Octave = "2",
+                                Duration = "6"
+                            },
+                            new NewNote
+                            {
+                                Step = "C",
+                                Alter = string.Empty,
+                                Octave = "2",
+                                Duration = "6"
+                            },
+                            new NewNote
+                            {
+                                Step = "E",
+                                Alter = "1",
+                                Octave = "2",
+                                Duration = "6"
+                            }
+                        }
+                    },
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                Step = "B",
+                                Alter = string.Empty,
+                                Octave = "2",
+                                Duration = "12"
+                            },
+                            new NewNote
+                            {
+                                Step = "D",
+                                Alter = string.Empty,
+                                Octave = "2",
+                                Duration = "12"
+                            },
+                            new NewNote
+                            {
+                                Step = "F",
+                                Alter = "1",
+                                Octave = "2",
+                                Duration = "12"
+                            }
+                        }
+                    }
+                }
+            });
+            return this;
+        }
+
+        public PartBuilder AddMeasureOfRests(string part, string voice)
+        {
+            _voices[(part, voice)].Add(new NewVoice
+            {
+                Chords = new List<NewChord>
+                {
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                IsRest = true,
+                                Duration = "6"
+                            }
+                        }
+                    },
+                    new NewChord
+                    {
+                        Notes = new List<NewNote>
+                        {
+                            new NewNote
+                            {
+                                IsRest = true,
+                                Duration = "12"
+                            }
+                        }
+                    }
+                }
+            });
+            return this;
+        }
+
+        public List<NewPart> Build()
+        {
+            var parts = new List<NewPart>();
+            var measureCount = _voices.First().Value.Count;
+            Assert.IsTrue(_voices.All(v => v.Value.Count == measureCount), "All voices must have the same number of measures");
+            foreach (var partAndVoices in _voices.Keys.GroupBy(k => k.part))
+            {
+                var part = new NewPart();
+                foreach (var partAndVoice in partAndVoices)
+                {
+                    for(var m = 0; m < measureCount; ++m)
+                    {
+                        part.Measures.Add(new NewMeasure
+                        {
+                            Voices = _voices
+                                .Where(v => partAndVoices.Contains(v.Key))
+                                .ToDictionary(
+                                    avftp => avftp.Key.voice,
+                                    avftp => avftp.Value[m]
+                                )
+                        });
+                    }
+                }
+                parts.Add(part);
+            }
+            return parts;
+        }
+    }
+}
