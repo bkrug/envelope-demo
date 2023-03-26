8f16b67da8b45e88060da88c5ff1c958de87a2de
Benjamin Krug <benjaminkrug@yahoo.com>
2023-03-26 16:22:20 -0400

    Add test that merged rests are represented properly

--- MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
@@ -13,7 +13,6 @@ namespace MusicXmlParser.Tests
     {
         private readonly Mock<ILogger> _logger = new Mock<ILogger>();
 
-        //TODO: Add a (shorter) test for situations where a single note crosses two measures
         //TODO: Why to we expect three REPT when only one tone generator is required?
         //     ORCHES DATA ORCH1,ORCH2,ORCH3
         //     * Data structures dealing with repeated music
@@ -374,6 +373,183 @@ ORCH1
        BYTE B0,N8
        BYTE REST,0      * Invalid: 
        BYTE A0,N16
+ORCH1A
+*
+
+";
+            var options = new Options
+            {
+                AsmLabel = "ORCHES",
+                Ratio60Hz = "2:1",
+                Ratio50Hz = "10:6",
+                RepetitionType = RepetitionType.RepeatFromBeginning
+            };
+            var memoryStream = new MemoryStream();
+
+            //Act
+            var streamWriter = new StreamWriter(memoryStream);
+            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            streamWriter.Flush();
+
+            //Assert
+            memoryStream.Position = 0;
+            using var streamReader = new StreamReader(memoryStream);
+            var actualText = streamReader.ReadToEnd();
+            actualText.Should().BeEquivalentTo(EXPECTED_TEXT);
+        }
+
+        [Test]
+        public void AssemblyMaker_RestsCanMerge_MeasuresAreCombined()
+        {
+            const string MUSIC_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE score-partwise PUBLIC ""-//Recordare//DTD MusicXML 3.1 Partwise//EN"" ""http://www.musicxml.org/dtds/partwise.dtd"">
+<score-partwise version=""3.1"">
+  <work>
+    <work-title>I Love Eggs</work-title>
+  </work>
+  <identification>
+    <creator type=""composer"">Somebody Somewhere</creator>
+    <source>http://whitehouse.gov</source>
+  </identification>
+  <part-list>
+    <score-part id=""P13"">
+    </score-part>
+  </part-list>
+  <part id=""P13"">
+    <measure number=""0"" implicit=""yes"" width=""147.40"">
+      <attributes>
+        <divisions>24</divisions>
+      </attributes>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <pitch>
+          <step>B</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+      <note default-x=""60.39"" default-y=""-35.00"">
+        <pitch>
+          <step>C</step>
+          <alter>1</alter>
+          <octave>3</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+    </measure>
+    <measure>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <pitch>
+          <step>A</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <rest />
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+    </measure>
+    <measure>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <rest />
+        <duration>12</duration>
+        <voice>1</voice>
+        <type>eighth</type>
+        <stem>down</stem>
+      </note>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <pitch>
+          <step>A</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <rest />
+        <duration>12</duration>
+        <voice>1</voice>
+        <type>eighth</type>
+        <stem>down</stem>
+      </note>
+    </measure>
+    <measure>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <rest />
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+      <note default-x=""81.48"" default-y=""-5.00"">
+        <pitch>
+          <step>A</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+        <type>quarter</type>
+        <stem>down</stem>
+      </note>
+    </measure>
+  </part>
+</score-partwise>";
+            const string EXPECTED_TEXT =
+@"       DEF  ORCHES
+
+*
+* This is auto-generated code.
+* It is only included in the repo for the convenience of people who haven't cloned it.
+*
+* I Love Eggs
+* Somebody Somewhere
+* Source: http://whitehouse.gov
+*
+
+       COPY 'NOTEVAL.asm'
+       COPY 'CONST.asm'
+
+*
+* Song Header
+*
+ORCHES DATA ORCH1,ORCH2,ORCH3
+* Data structures dealing with repeated music
+       DATA REPT1,REPT2,REPT3
+* Duration ratio in 60hz environment
+       DATA 2,1
+* Duration ratio in 50hz environment
+       DATA 10,6
+
+REPT1
+       DATA ORCH1A,ORCH1
+       DATA REPEAT,REPT1
+
+* Generator 1
+* Measure 1
+ORCH1
+       BYTE B0,N4
+       BYTE Cs1,N4
+* Measure 2 - 4
+       BYTE A0,N4
+       BYTE REST,N4DOT
+       BYTE A0,N4
+       BYTE REST,N4DOT
+       BYTE A0,N4
 ORCH1A
 *
 
