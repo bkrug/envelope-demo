d183bbd574c22e44eb8fbbaef63ae0c7994ad4ba
Benjamin Krug <benjaminkrug@yahoo.com>
2023-04-07 21:50:38 -0400

    Add Division Tests

--- MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
@@ -10,13 +10,6 @@ namespace MusicXmlParser.Tests
 {
     public class AssemblyMakerTests
     {
-        private readonly Mock<ILogger> _logger = new Mock<ILogger>();
-
-        private AssemblyMaker GetAssemblyMaker()
-        {
-            return new AssemblyMaker(new NoteParser(_logger.Object), new SN76489NoteGenerator(_logger.Object), new AssemblyWriter());
-        }
-
         [Test]
         public void AssemblyMaker_RestsCanMerge_MeasuresAreCombined()
         {
@@ -168,206 +161,7 @@ ORCH1A
 
             //Act
             var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
-            streamWriter.Flush();
-
-            //Assert
-            memoryStream.Position = 0;
-            using var streamReader = new StreamReader(memoryStream);
-            var actualText = streamReader.ReadToEnd();
-            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
-        }
-
-        [Test]
-        public void AssemblyMaker_CreditInformationSupplied_CreditInformationInOutput()
-        {
-            const string MUSIC_XML =
-@"<?xml version=""1.0"" encoding=""UTF-8""?>
-<!DOCTYPE score-partwise PUBLIC ""-//Recordare//DTD MusicXML 3.1 Partwise//EN"" ""http://www.musicxml.org/dtds/partwise.dtd"">
-<score-partwise version=""3.1"">
-  <work>
-    <work-title>I Love Eggs</work-title>
-  </work>
-  <identification>
-    <creator type=""composer"">Somebody Somewhere</creator>
-    <source>http://whitehouse.gov</source>
-  </identification>
-  <part-list>
-    <score-part id=""P13"">
-    </score-part>
-  </part-list>
-  <part id=""P13"">
-    <measure number=""0"" implicit=""yes"" width=""147.40"">
-      <attributes>
-        <divisions>24</divisions>
-      </attributes>
-      <note default-x=""81.48"" default-y=""-5.00"">
-        <pitch>
-          <step>B</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>12</duration>
-        <voice>1</voice>
-        <type>eighth</type>
-        <stem>down</stem>
-      </note>
-      <note default-x=""81.48"" default-y=""-5.00"">
-        <pitch>
-          <step>A</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>6</duration>
-        <voice>1</voice>
-        <type>16th</type>
-        <stem>down</stem>
-      </note>
-    </measure>
-  </part>
-</score-partwise>";
-            const string EXPECTED_TEXT =
-@"       DEF  ORCHES
-
-*
-* I Love Eggs
-* Somebody Somewhere
-* Source: http://whitehouse.gov
-*
-
-       COPY 'NOTEVAL.asm'
-       COPY 'CONST.asm'
-
-*
-* Song Header
-*
-ORCHES DATA ORCH1,0,0
-* Data structures dealing with repeated music
-       DATA REPT1,0,0
-* Duration ratio in 60hz environment
-       DATA 2,1
-* Duration ratio in 50hz environment
-       DATA 10,6
-
-REPT1
-       DATA ORCH1A,ORCH1
-       DATA REPEAT,REPT1
-
-* Generator 1
-* Measure 1
-ORCH1
-       BYTE B0,N8
-       BYTE A0,N16
-ORCH1A
-*
-
-";
-            var options = new Options
-            {
-                AsmLabel = "ORCHES",
-                Ratio60Hz = "2:1",
-                Ratio50Hz = "10:6",
-                RepetitionType = RepetitionType.RepeatFromBeginning
-            };
-            var memoryStream = new MemoryStream();
-
-            //Act
-            var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
-            streamWriter.Flush();
-
-            //Assert
-            memoryStream.Position = 0;
-            using var streamReader = new StreamReader(memoryStream);
-            var actualText = streamReader.ReadToEnd();
-            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
-        }
-
-        [Test]
-        public void AssemblyMaker_IncludeCompilerWarningSupplied_WarningInOutput()
-        {
-            const string MUSIC_XML =
-@"<?xml version=""1.0"" encoding=""UTF-8""?>
-<!DOCTYPE score-partwise PUBLIC ""-//Recordare//DTD MusicXML 3.1 Partwise//EN"" ""http://www.musicxml.org/dtds/partwise.dtd"">
-<score-partwise version=""3.1"">
-  <part-list>
-    <score-part id=""P13"">
-    </score-part>
-  </part-list>
-  <part id=""P13"">
-    <measure number=""0"" implicit=""yes"" width=""147.40"">
-      <attributes>
-        <divisions>24</divisions>
-      </attributes>
-      <note default-x=""81.48"" default-y=""-5.00"">
-        <pitch>
-          <step>B</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>12</duration>
-        <voice>1</voice>
-        <type>eighth</type>
-        <stem>down</stem>
-      </note>
-      <note default-x=""81.48"" default-y=""-5.00"">
-        <pitch>
-          <step>A</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>6</duration>
-        <voice>1</voice>
-        <type>16th</type>
-        <stem>down</stem>
-      </note>
-    </measure>
-  </part>
-</score-partwise>";
-            const string EXPECTED_TEXT =
-@"       DEF  ORCHES
-
-*
-* This is auto-generated code.
-* It is only included in the repo for the convenience of people who haven't cloned it.
-*
-
-       COPY 'NOTEVAL.asm'
-       COPY 'CONST.asm'
-
-*
-* Song Header
-*
-ORCHES DATA ORCH1,0,0
-* Data structures dealing with repeated music
-       DATA REPT1,0,0
-* Duration ratio in 60hz environment
-       DATA 2,1
-* Duration ratio in 50hz environment
-       DATA 10,6
-
-REPT1
-       DATA ORCH1A,ORCH1
-       DATA REPEAT,REPT1
-
-* Generator 1
-* Measure 1
-ORCH1
-       BYTE B0,N8
-       BYTE A0,N16
-ORCH1A
-*
-
-";
-            var options = new Options
-            {
-                AsmLabel = "ORCHES",
-                Ratio60Hz = "2:1",
-                Ratio50Hz = "10:6",
-                RepetitionType = RepetitionType.RepeatFromBeginning,
-                DisplayRepoWarning = true
-            };
-            var memoryStream = new MemoryStream();
-
-            //Act
-            var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
             streamWriter.Flush();
 
             //Assert
@@ -489,7 +283,7 @@ ORCH2A
 
             //Act
             var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
             streamWriter.Flush();
 
             //Assert
@@ -611,7 +405,7 @@ ORCH2A
 
             //Act
             var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
             streamWriter.Flush();
 
             //Assert
@@ -754,7 +548,7 @@ ORCH2A
 
             //Act
             var streamWriter = new StreamWriter(memoryStream);
-            GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
             streamWriter.Flush();
 
             //Assert
---
+++ MusicXmlParser/MusicXmlParser.Tests/DivisionsTests.cs
@@ -0,0 +1,221 @@
+﻿using MusicXmlParser.Enums;
+using MusicXmlParser.Models;
+using NUnit.Framework;
+using System.IO;
+using System.Xml.Linq;
+
+namespace MusicXmlParser.Tests
+{
+    public class DivisionsTests
+    {
+        [Test]
+        public void Divisions_QuarterNoteHasLengthOf24_32ndHasLengthOf3()
+        {
+            const string MUSIC_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE score-partwise PUBLIC ""-//Recordare//DTD MusicXML 3.1 Partwise//EN"" ""http://www.musicxml.org/dtds/partwise.dtd"">
+<score-partwise version=""3.1"">
+  <part-list>
+    <score-part id=""P13"">
+    </score-part>
+  </part-list>
+  <part id=""P13"">
+    <measure number=""0"">
+      <attributes>
+        <divisions>24</divisions>
+      </attributes>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>24</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>A</step>
+          <alter>1</alter>
+          <octave>2</octave>
+          </pitch>
+        <duration>3</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>2</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>A</step>
+          <octave>2</octave>
+          </pitch>
+        <duration>2</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>2</duration>
+        <voice>5</voice>
+      </note>
+    </measure>
+  </part>
+</score-partwise>";
+            const string EXPECTED_TEXT =
+@"       DEF  MELDY
+
+       COPY 'NOTEVAL.asm'
+       COPY 'CONST.asm'
+
+*
+* Song Header
+*
+MELDY  DATA MELD1,0,0
+* Data structures dealing with repeated music
+       DATA REPT1,0,0
+* Duration ratio in 60hz environment
+       DATA 2,1
+* Duration ratio in 50hz environment
+       DATA 10,6
+
+REPT1
+       DATA MELD1A,MELD1
+       DATA REPEAT,REPT1
+
+* Generator 1
+* Measure 1
+MELD1
+       BYTE D1,N4
+       BYTE As0,N32
+       BYTE D1,N32TRP
+       BYTE A0,N32TRP
+       BYTE D1,N32TRP
+MELD1A
+*
+
+";
+            var options = new Options
+            {
+                AsmLabel = "MELDY",
+                Ratio60Hz = "2:1",
+                Ratio50Hz = "10:6",
+                RepetitionType = RepetitionType.RepeatFromBeginning
+            };
+            var memoryStream = new MemoryStream();
+
+            //Act
+            var streamWriter = new StreamWriter(memoryStream);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            streamWriter.Flush();
+
+            //Assert
+            memoryStream.Position = 0;
+            using var streamReader = new StreamReader(memoryStream);
+            var actualText = streamReader.ReadToEnd();
+            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
+        }
+
+        [Test]
+        public void Divisions_QuarterNoteHasLengthOf8_32ndHasLengthOf1()
+        {
+            const string MUSIC_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE score-partwise PUBLIC ""-//Recordare//DTD MusicXML 3.1 Partwise//EN"" ""http://www.musicxml.org/dtds/partwise.dtd"">
+<score-partwise version=""3.1"">
+  <part-list>
+    <score-part id=""P13"">
+    </score-part>
+  </part-list>
+  <part id=""P13"">
+    <measure number=""0"">
+      <attributes>
+        <divisions>8</divisions>
+      </attributes>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>A</step>
+          <alter>1</alter>
+          <octave>2</octave>
+          </pitch>
+        <duration>1</duration>
+        <voice>5</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>32</duration>
+        <voice>5</voice>
+      </note>
+    </measure>
+  </part>
+</score-partwise>";
+            const string EXPECTED_TEXT =
+@"       DEF  MELDY
+
+       COPY 'NOTEVAL.asm'
+       COPY 'CONST.asm'
+
+*
+* Song Header
+*
+MELDY  DATA MELD1,0,0
+* Data structures dealing with repeated music
+       DATA REPT1,0,0
+* Duration ratio in 60hz environment
+       DATA 2,1
+* Duration ratio in 50hz environment
+       DATA 10,6
+
+REPT1
+       DATA MELD1A,MELD1
+       DATA REPEAT,REPT1
+
+* Generator 1
+* Measure 1
+MELD1
+       BYTE D1,N4
+       BYTE As0,N32
+       BYTE D1,N1
+MELD1A
+*
+
+";
+            var options = new Options
+            {
+                AsmLabel = "MELDY",
+                Ratio60Hz = "2:1",
+                Ratio50Hz = "10:6",
+                RepetitionType = RepetitionType.RepeatFromBeginning
+            };
+            var memoryStream = new MemoryStream();
+
+            //Act
+            var streamWriter = new StreamWriter(memoryStream);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            streamWriter.Flush();
+
+            //Assert
+            memoryStream.Position = 0;
+            using var streamReader = new StreamReader(memoryStream);
+            var actualText = streamReader.ReadToEnd();
+            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
+        }
+    }
+}
