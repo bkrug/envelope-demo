51de93386a791d2c0376bf263da131e34bd319e3
Benjamin Krug <benjaminkrug@yahoo.com>
2023-04-13 21:58:27 -0400

    Add test for situation in which late measures have fewer voices

--- MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/AssemblyMakerTests.cs
@@ -242,9 +242,144 @@ ORCH2A
             TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
         }
 
-        //TODO: Write a similar test in which the Last Measure has Fewer Voices Than the First Measure
         [Test]
-        public void GroupByGenerator_FirstMeasureHasFewerVoicesThanNextMeasure_NoNullReferenceErrors()
+        public void AssemblyMaker_BackupDoesNotReturnToMeasureStart_RestInsertedBeforeNotes()
+        {
+            const string MUSIC_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<score-partwise>
+  <part id=""P1"">
+    <measure number=""0"">
+      <attributes>
+        <divisions>24</divisions>
+      </attributes>
+      <note>
+        <pitch>
+          <step>A</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+      <backup>
+        <duration>24</duration>
+      </backup>
+      <note>
+        <pitch>
+          <step>A</step>
+          <alter>1</alter>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>2</voice>
+      </note>
+    </measure>
+    <measure number=""1"">
+      <note>
+        <pitch>
+          <step>B</step>
+          <octave>2</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>3</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+      <backup>
+        <duration>36</duration>
+      </backup>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>3</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>2</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>E</step>
+          <octave>3</octave>
+        </pitch>
+        <duration>12</duration>
+        <voice>2</voice>
+      </note>
+    </measure>
+  </part>
+</score-partwise>";
+            const string EXPECTED_TEXT =
+@"       DEF  ORCHES
+
+       COPY 'NOTEVAL.asm'
+       COPY 'CONST.asm'
+
+*
+* Song Header
+*
+ORCHES DATA ORCH1,ORCH2,0
+* Data structures dealing with repeated music
+       DATA REPT1,REPT2,0
+* Duration ratio in 60hz environment
+       DATA 2,1
+* Duration ratio in 50hz environment
+       DATA 10,6
+
+REPT1
+       DATA ORCH1A,ORCH1
+       DATA REPEAT,REPT1
+REPT2
+       DATA ORCH2A,ORCH2
+       DATA REPEAT,REPT2
+
+* Generator 1
+* Measure 1
+ORCH1
+       BYTE A0,N4
+* Measure 2
+       BYTE B0,N4
+       BYTE C1,N4
+ORCH1A
+*
+
+* Generator 2
+* Measure 1
+ORCH2
+       BYTE As0,N4
+* Measure 2
+       BYTE REST,N8
+       BYTE D1,N4
+       BYTE E1,N8
+ORCH2A
+*
+
+";
+            var options = new Options
+            {
+                AsmLabel = "ORCHES",
+                Ratio60Hz = "2:1",
+                Ratio50Hz = "10:6",
+                RepetitionType = RepetitionType.RepeatFromBeginning
+            };
+            var instantiator = new AssemblyMakerInstantiator();
+
+            //Act
+            var streamWriter = new StreamWriter(instantiator.MemoryStream);
+            instantiator.GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            streamWriter.Flush();
+
+            //Assert
+            var actualText = instantiator.GetContentsOfMemoryStream();
+            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
+        }
+
+        [Test]
+        public void GroupByGenerator_FirstMeasuresHaveFewerVoicesThanLastMeasure_NoNullReferenceErrors()
         {
             const string MUSIC_XML =
 @"<?xml version=""1.0"" encoding=""UTF-8""?>
@@ -470,7 +605,7 @@ ORCH3A
         }
 
         [Test]
-        public void AssemblyMaker_BackupDoesNotReturnToMeasureStart_RestInsertedBeforeNotes()
+        public void GroupByGenerator_LastMeasuresHaveFewerVoicesThanFirstMeasure_NoNullReferenceErrors()
         {
             const string MUSIC_XML =
 @"<?xml version=""1.0"" encoding=""UTF-8""?>
@@ -478,64 +613,132 @@ ORCH3A
   <part id=""P1"">
     <measure number=""0"">
       <attributes>
-        <divisions>24</divisions>
+        <divisions>2</divisions>
       </attributes>
       <note>
         <pitch>
-          <step>A</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>24</duration>
+          <step>C</step>
+          <octave>4</octave>
+          </pitch>
+        <duration>8</duration>
         <voice>1</voice>
+        <type>whole</type>
       </note>
       <backup>
-        <duration>24</duration>
+        <duration>8</duration>
+        </backup>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>4</octave>
+        </pitch>
+        <duration>4</duration>
+        <voice>3</voice>
+        <type>half</type>
+      </note>
+      <backup>
+        <duration>4</duration>
       </backup>
       <note>
         <pitch>
-          <step>A</step>
-          <alter>1</alter>
-          <octave>2</octave>
-        </pitch>
-        <duration>24</duration>
-        <voice>2</voice>
+          <step>F</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+        <type>whole</type>
       </note>
     </measure>
     <measure number=""1"">
       <note>
-        <pitch>
-          <step>B</step>
-          <octave>2</octave>
-        </pitch>
-        <duration>24</duration>
+        <rest/>
+        <duration>8</duration>
         <voice>1</voice>
-      </note>
+        <type>whole</type>
+        <staff>1</staff>
+        </note>
+      <backup>
+        <duration>8</duration>
+        </backup>
       <note>
         <pitch>
           <step>C</step>
           <octave>3</octave>
-        </pitch>
-        <duration>24</duration>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+        <type>whole</type>
+        <stem>up</stem>
+        <staff>2</staff>
+      </note>
+    </measure>
+    <measure number=""2"">
+      <note>
+        <rest/>
+        <duration>8</duration>
         <voice>1</voice>
-      </note>
+        <type>whole</type>
+        <staff>1</staff>
+        </note>
       <backup>
-        <duration>36</duration>
-      </backup>
+        <duration>8</duration>
+        </backup>
       <note>
         <pitch>
-          <step>D</step>
+          <step>C</step>
           <octave>3</octave>
-        </pitch>
-        <duration>24</duration>
-        <voice>2</voice>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+        <type>whole</type>
+        <stem>up</stem>
+        <staff>2</staff>
       </note>
+    </measure>
+    <measure number=""3"">
+      <note>
+        <rest/>
+        <duration>8</duration>
+        <voice>1</voice>
+        <type>whole</type>
+        <staff>1</staff>
+        </note>
+      <backup>
+        <duration>8</duration>
+        </backup>
       <note>
         <pitch>
-          <step>E</step>
+          <step>C</step>
           <octave>3</octave>
-        </pitch>
-        <duration>12</duration>
-        <voice>2</voice>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+        <type>whole</type>
+        <stem>up</stem>
+        <staff>2</staff>
+      </note>
+    </measure>
+    <measure number=""4"">
+      <note>
+        <rest/>
+        <duration>8</duration>
+        <voice>1</voice>
+        <type>whole</type>
+        <staff>1</staff>
+        </note>
+      <backup>
+        <duration>8</duration>
+        </backup>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>3</octave>
+          </pitch>
+        <duration>8</duration>
+        <voice>5</voice>
+        <type>whole</type>
+        <stem>up</stem>
+        <staff>2</staff>
       </note>
     </measure>
   </part>
@@ -549,9 +752,9 @@ ORCH3A
 *
 * Song Header
 *
-ORCHES DATA ORCH1,ORCH2,0
+ORCHES DATA ORCH1,ORCH2,ORCH3
 * Data structures dealing with repeated music
-       DATA REPT1,REPT2,0
+       DATA REPT1,REPT2,REPT3
 * Duration ratio in 60hz environment
        DATA 2,1
 * Duration ratio in 50hz environment
@@ -563,26 +766,46 @@ REPT1
 REPT2
        DATA ORCH2A,ORCH2
        DATA REPEAT,REPT2
+REPT3
+       DATA ORCH3A,ORCH3
+       DATA REPEAT,REPT3
 
 * Generator 1
 * Measure 1
 ORCH1
-       BYTE A0,N4
+       BYTE C2,N1
 * Measure 2
-       BYTE B0,N4
-       BYTE C1,N4
+       BYTE C1,N1
+* Measure 3
+       BYTE C1,N1
+* Measure 4
+       BYTE C1,N1
+* Measure 5
+       BYTE C1,N1
 ORCH1A
 *
 
 * Generator 2
 * Measure 1
 ORCH2
-       BYTE As0,N4
-* Measure 2
-       BYTE REST,N8
-       BYTE D1,N4
-       BYTE E1,N8
+       BYTE D2,N2
+       BYTE REST,N2
+* Measure 2 - 3
+       BYTE REST,NDBL
+* Measure 4 - 5
+       BYTE REST,NDBL
 ORCH2A
+*
+
+* Generator 3
+* Measure 1
+ORCH3
+       BYTE F1,N1
+* Measure 2 - 3
+       BYTE REST,NDBL
+* Measure 4 - 5
+       BYTE REST,NDBL
+ORCH3A
 *
 
 ";
@@ -593,15 +816,17 @@ ORCH2A
                 Ratio50Hz = "10:6",
                 RepetitionType = RepetitionType.RepeatFromBeginning
             };
-            var instantiator = new AssemblyMakerInstantiator();
+            var memoryStream = new MemoryStream();
 
             //Act
-            var streamWriter = new StreamWriter(instantiator.MemoryStream);
-            instantiator.GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            var streamWriter = new StreamWriter(memoryStream);
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
             streamWriter.Flush();
 
             //Assert
-            var actualText = instantiator.GetContentsOfMemoryStream();
+            memoryStream.Position = 0;
+            using var streamReader = new StreamReader(memoryStream);
+            var actualText = streamReader.ReadToEnd();
             TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
         }
     }
