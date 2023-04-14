6b9f808be4ba77e42ca2a664267663fde8bd31e5
Benjamin Krug <benjaminkrug@yahoo.com>
2023-04-13 23:01:42 -0400

    Add more snapshot tests

--- MusicXmlParser/MusicXmlParser.Tests/SnapshotTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/SnapshotTests.cs
@@ -34,5 +34,51 @@ namespace MusicXmlParser.Tests
             var actualText = File.ReadAllText(options.OutputFile);
             TextAsserts.EquivalentLines(expectedText, actualText);
         }
+
+        [Test]
+        public void AssemblyMaker_MerryFarmer_Snapshot()
+        {
+            var options = new Options
+            {
+                InputFile = "Files//Schumann_The_Merry_Farmer_Op._68_No._10.musicxml",
+                OutputFile = "Files//actual_TUNEFARMER.asm",
+                AsmLabel = "SCHUMN",
+                Ratio60Hz = "5:4",
+                Ratio50Hz = "1:1",
+                RepetitionType = RepetitionType.StopAtEnd,
+                DisplayRepoWarning = true
+            };
+
+            //Act
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options);
+
+            //Assert
+            var expectedText = File.ReadAllText("Files//TUNEFARMER.asm");
+            var actualText = File.ReadAllText(options.OutputFile);
+            TextAsserts.EquivalentLines(expectedText, actualText);
+        }
+
+        [Test]
+        public void AssemblyMaker_OldFolksAtHome_Snapshot()
+        {
+            var options = new Options
+            {
+                InputFile = "Files//Old_Folks_At_Home_-_Theme_and_Variations_by_Stephen_Foster.musicxml",
+                OutputFile = "Files//actual_TUNEOLDFOLKS.asm",
+                AsmLabel = "FOSTER",
+                Ratio60Hz = "5:4",
+                Ratio50Hz = "1:1",
+                RepetitionType = RepetitionType.RepeatFromFirstJump,
+                DisplayRepoWarning = false
+            };
+
+            //Act
+            new AssemblyMakerInstantiator().GetAssemblyMaker().ConvertToAssembly(options);
+
+            //Assert
+            var expectedText = File.ReadAllText("Files//TUNEOLDFOLKS.asm");
+            var actualText = File.ReadAllText(options.OutputFile);
+            TextAsserts.EquivalentLines(expectedText, actualText);
+        }
     }
 }
