fbbfe534729497703967d1598c0fd15404003502
Benjamin Krug <benjaminkrug@yahoo.com>
2023-04-15 12:53:44 -0400

    Rewrite TwoPartsOneVoiceEach test

--- MusicXmlParser/MusicXmlParser.Tests/GroupByGeneratorTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/GroupByGeneratorTests.cs
@@ -131,6 +131,145 @@ ORCH1A
         }
 
         [Test]
+        public void GroupByGenerator_TwoPartsOneVoiceEachNoChords_TwoGeneratorsOutput()
+        {
+            const string MUSIC_XML =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<score-partwise>
+  <part id=""P1"">
+    <measure>
+      <attributes>
+        <divisions>24</divisions>
+      </attributes>
+      <note>
+        <pitch>
+          <step>B</step>
+          <alter>-1</alter>
+          <octave>3</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>4</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+    </measure>
+    <measure>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>4</octave>
+        </pitch>
+        <duration>48</duration>
+        <voice>1</voice>
+      </note>
+    </measure>
+  </part>
+  <part id=""P2"">
+    <measure>
+      <attributes>
+        <divisions>24</divisions>
+      </attributes>
+      <note>
+        <pitch>
+          <step>B</step>
+          <alter>-1</alter>
+          <octave>4</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>5</octave>
+        </pitch>
+        <duration>24</duration>
+        <voice>1</voice>
+      </note>
+    </measure>
+    <measure>
+      <note>
+        <pitch>
+          <step>D</step>
+          <octave>5</octave>
+        </pitch>
+        <duration>48</duration>
+        <voice>1</voice>
+      </note>
+    </measure>
+  </part>
+</score-partwise>";
+            const string EXPECTED_TEXT =
+@"       DEF  ORCHES
+
+       COPY 'NOTEVAL.asm'
+       COPY 'CONST.asm'
+
+*
+* Song Header
+*
+ORCHES DATA ORCH1,ORCH2,0
+* Data structures dealing with repeated music
+       DATA REPT1,REPT2,0
+* Duration ratio in 60hz environment
+       DATA 2,1
+* Duration ratio in 50hz environment
+       DATA 10,6
+
+REPT1
+       DATA ORCH1A,ORCH1
+       DATA REPEAT,REPT1
+REPT2
+       DATA ORCH2A,ORCH2
+       DATA REPEAT,REPT2
+
+* Generator 1
+* Measure 1
+ORCH1
+       BYTE Bb1,N4
+       BYTE C2,N4
+* Measure 2
+       BYTE D2,N2
+ORCH1A
+*
+
+* Generator 2
+* Measure 1
+ORCH2
+       BYTE Bb2,N4
+       BYTE C3,N4
+* Measure 2
+       BYTE D3,N2
+ORCH2A
+*
+
+";
+            var options = new Options
+            {
+                AsmLabel = "ORCHES",
+                Ratio60Hz = "2:1",
+                Ratio50Hz = "10:6",
+                RepetitionType = RepetitionType.RepeatFromBeginning
+            };
+            var instantiator = new AssemblyMakerInstantiator();
+
+            //Act
+            var streamWriter = new StreamWriter(instantiator.MemoryStream);
+            instantiator.GetAssemblyMaker().ConvertToAssembly(options, XDocument.Parse(MUSIC_XML), ref streamWriter);
+            streamWriter.Flush();
+
+            //Assert
+            var actualText = instantiator.GetContentsOfMemoryStream();
+            TextAsserts.EquivalentLines(EXPECTED_TEXT, actualText);
+        }
+
+        [Test]
         public void GroupByGenerator_FirstMeasuresHaveFewerVoicesThanLastMeasure_NoNullReferenceErrors()
         {
             const string MUSIC_XML =
--- MusicXmlParser/MusicXmlParser.Tests/ToneGeneratorGroupingTests.cs
+++ MusicXmlParser/MusicXmlParser.Tests/ToneGeneratorGroupingTests.cs
@@ -23,43 +23,6 @@ namespace MusicXmlParser.Tests
         }
 
         [Test]
-        public void GroupByGenerator_TwoPartsNoChords_Success()
-        {
-            var singlePartTwoVoices = new PartBuilder()
-                .AddPartAndVoice("p1", "v1")
-                .AddMeasureOfOneNoteChords("p1", "v1")
-                .AddMeasureOfOneNoteChords("p1", "v1")
-                .AddPartAndVoice("p2", "v3")
-                .AddMeasureOfOneNoteChords("p2", "v3")
-                .AddMeasureOfOneNoteChords("p2", "v3")
-                .Build();
-            var expectedToneGenerators = new List<ToneGenerator>()
-            {
-                new ToneGenerator
-                {
-                    GeneratorNotes =
-                        GetMeasureOfGeneratorNotes(1)
-                        .Concat(GetMeasureOfGeneratorNotes(2))
-                        .ToList()
-                },
-                new ToneGenerator
-                {
-                    GeneratorNotes =
-                        GetMeasureOfGeneratorNotes(1)
-                        .Concat(GetMeasureOfGeneratorNotes(2))
-                        .ToList()
-                }
-            };
-            AddDetailsConsistentWithNonRepeatingSong(expectedToneGenerators);
-
-            //Act
-            var actualToneGenerators = GetGenerator().GetToneGenerators(singlePartTwoVoices, "LBL", _defaultOptions);
-
-            //Assert
-            actualToneGenerators.Should().BeEquivalentTo(expectedToneGenerators);
-        }
-
-        [Test]
         public void GroupByGenerator_TwoVoicesNoChords_Success()
         {
             var singlePartTwoVoices = new PartBuilder()
